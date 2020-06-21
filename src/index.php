5d68c8e0512153471dc9c0b490a2e8a0552666ef
gadeach <gadeach@globalhitss.com>
2020-06-21 20:02:28 -0300

    new changes

--- src/index.php
+++ src/index.php
@@ -5,12 +5,31 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Crox Notices</title>
+        <title>C-rox Notices</title>
     </head>
     <body>
-        <header class="header">Welcome to Crox Notices! The truth is with us!</header>
-        <main class="main">
-
+        <header id="header">
+            <nav id="menu">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+            <a href="#" id="login">Login</a>
+            <a href="#" id="register">Register</a>
+            <h1>Welcome to Crox Notices! The truth is with us!</h1>
+        </header>
+        <main id="main">
+            <aside id="logo">
+                LOGO
+            </aside>
+            <div id="central">
+                CENTRAL
+            </div>
         </main>
         <?php include_once "view/partial/footer.php"; ?>
     </body>
--- src/view/UserView.php
+++ src/view/UserView.php
@@ -8,7 +8,9 @@
         <title>User</title>
     </head>
     <body>
-        <header></header>
+        <header>
+            <h1>Welcome {$username}</h1>
+        </header>
         <main></main>
         <?php   include_once "partial/footer.php";  ?>
     </body>
