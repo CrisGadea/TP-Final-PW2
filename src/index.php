afa43002754db35d6553143fdefc17ad84defe91
gadeach <gadeach@globalhitss.com>
2020-06-21 23:40:49 -0300

    added header mustache

--- src/index.php
+++ src/index.php
@@ -1,10 +1,4 @@
-<!doctype html>
-<html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<?php include_once "view/partial/header.mustache"; ?>
         <title>C-rox Notices</title>
     </head>
     <body>
@@ -31,6 +25,6 @@
                 CENTRAL
             </div>
         </main>
-        <?php include_once "view/partial/footer.php"; ?>
+        <?php include_once "view/partial/footer.mustache"; ?>
     </body>
 </html>
--- src/view/UserView.php
+++ src/view/UserView.php
@@ -1,10 +1,4 @@
-<!doctype html>
-<html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<?php include_once "view/partial/header.mustache"; ?>
         <title>User</title>
     </head>
     <body>
@@ -12,6 +6,6 @@
             <h1>Welcome {$username}</h1>
         </header>
         <main></main>
-        <?php   include_once "partial/footer.php";  ?>
+        <?php   include_once "partial/footer.mustache";  ?>
     </body>
 </html>
