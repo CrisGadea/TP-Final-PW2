64a1ad92332fa506a988974a7a35482a0098ef33
gadeach <gadeach@globalhitss.com>
2020-06-24 15:22:05 -0300

    Added changes

---
+++ app/ModuleInitializer.php
@@ -0,0 +1,10 @@
+<?php
+require_once("helper/Renderer.php");
+include_once("helper/Database.php");
+include_once("helper/Config.php");
+require_once('third-party/mustache/src/Mustache/Autoloader.php');
+
+class ModuleInitializer
+{
+
+}
