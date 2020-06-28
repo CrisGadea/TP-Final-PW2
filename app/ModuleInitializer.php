fa736c8dfb40bfd640d74efd3cabdcb385c7fc1e
gadeach <gadeach@globalhitss.com>
2020-06-27 21:10:43 -0300

    fix

--- app/ModuleInitializer.php
+++ app/ModuleInitializer.php
@@ -2,7 +2,7 @@
 require_once("helper/Renderer.php");
 include_once("helper/Database.php");
 include_once("helper/Config.php");
-require_once('third-party/mustache/src/Mustache/Autoloader.php');
+require_once("third-party/mustache/src/Mustache/Autoloader.php");
 
 class ModuleInitializer
 {
